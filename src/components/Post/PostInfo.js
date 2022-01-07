a12072d77d532fdd241a7573b3842fe5af9490ce
kareem nour <kimzcodes@gmail.com>
2022-01-07 21:02:13 +0200

    init ui

--- src/components/Post/PostInfo.js
+++ src/components/Post/PostInfo.js
@@ -4,6 +4,14 @@ const Postinfo = () => {
   return (
     <Fragment>
       <h2>Post Details</h2>
+      <div class='alert alert-secondary' role='alert'>
+        There is no post selected yet. Please select!
+      </div>
+      {/* <div>
+        <p class='fw-bold'>Title:</p>
+        <p class='fw-light'>Description:</p>
+        <p class='fst-italic'>Price:</p>
+      </div> */}
     </Fragment>
   );
 };
