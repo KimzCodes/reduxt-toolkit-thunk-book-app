27722d0921e3f036611467c49e8cc7038f319fab
kareem nour <kimzcodes@gmail.com>
2022-01-09 22:18:05 +0200

    fix class

--- src/components/Post/PostInfo.js
+++ src/components/Post/PostInfo.js
@@ -4,13 +4,13 @@ const PostInfo = () => {
   return (
     <Fragment>
       <h2>Post Details</h2>
-      <div class='alert alert-secondary' role='alert'>
+      <div className='alert alert-secondary' role='alert'>
         There is no post selected yet. Please select!
       </div>
       {/* <div>
-        <p class='fw-bold'>Title:</p>
-        <p class='fw-light'>Description:</p>
-        <p class='fst-italic'>Price:</p>
+        <p className='fw-bold'>Title:</p>
+        <p className='fw-light'>Description:</p>
+        <p className='fst-italic'>Price:</p>
       </div> */}
     </Fragment>
   );
